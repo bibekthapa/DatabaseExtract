2be45dcabd8ae25d8ef23b799166861ca48ab154
Bibek <bibek.thapahere@gmail.com>
2016-10-15 07:59:24 -0700

    Inserted the data into the Database

--- src/com/example/DatabaseExtract/DatabaseConnection/DbConnection.java
+++ src/com/example/DatabaseExtract/DatabaseConnection/DbConnection.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.DatabaseExtract.DatabaseConnection;
 
-/**
- *
- * @author HOME
- */
-public class NewClass {
+import com.example.DatabaseExtract.constant.DBconstant;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnection {
+    private Connection conn=null;
+    private PreparedStatement stmt=null;
+   
+    
+    public void open() throws ClassNotFoundException, SQLException
+    {
+        Class.forName(DBconstant.DB_URL);
+        conn=DriverManager.getConnection(DBconstant.DB_URL, DBconstant.DB_Username, DBconstant.DB_Password);
+        System.out.println("Database is connected");
+        
+    }
+    public void close() throws SQLException
+    {
+            if(conn!=null && !conn.isClosed())
+            {
+                conn.close();
+                conn=null;
+            }
+    }
+     public PreparedStatement init() throws SQLException
+     {
+         stmt=(PreparedStatement) conn.createStatement();
+         return stmt;
+     }
+     public ResultSet execute(String query) throws SQLException
+     {
+          return stmt.executeQuery(query);
+     }
+    
+    
     
 }
--- src/com/example/DatabaseExtract/Program.java
+++ src/com/example/DatabaseExtract/Program.java
@@ -3,9 +3,11 @@ package com.example.DatabaseExtract;
 import com.example.DatabaseExtract.constant.DBconstant;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Program {
 
@@ -18,32 +20,60 @@ public class Program {
 
             conn = DriverManager.getConnection(DBconstant.DB_URL, DBconstant.DB_Username, DBconstant.DB_Password);
             System.out.println("Database is connected");
-            viewTable(conn);
+          //  viewTable(conn);
+            insertTable(conn);
 
             conn.close();
+            
 
         } catch (Exception ioe) {
             System.out.println("Not connected to DB: error:" + ioe);
 
         }
+        
+    }
+
+    public static void viewTable(Connection conn) {
+        Statement stmt = null;
+        String query = "Select client_description,client_id,client_name from project_management" + ".clients";
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int clientId = rs.getInt("client_id");
+                String clientName = rs.getString("client_name");
+                String clientDescription = rs.getString("client_description");
+                System.out.println("clientID:" + "\t" + clientId + "  clientName" + "\t" + clientName + "  clientDescription" + "\t" + clientDescription);
+
+            }
+
+        } catch (SQLException dberror) {
+
         }
+
+    }
     
-     public  static void viewTable(Connection conn)
-        {
-            Statement stmt=null;
-            String query="Select * from project_management"+".clients";
-            try
+    public  static void insertTable(Connection conn)
             {
-                stmt=conn.createStatement();
-                ResultSet rs= stmt.executeQuery(query);
-                while(rs.next())
-                {
-                    int clientId=rs.getInt("client_id");
-                    String clientName=rs.getString("client_name");
-                    String clientDescription=rs.getString("client_description");
-                    System.out.println("clientID:"+"\t"+clientId+"  clientName"+"\t"+clientName+"  clientDescription"+"\t"+clientDescription);
+                Scanner reader = new Scanner(System.in);
+            //stmt=null;
+           
+           System.out.println("Enter the client name");
+           String clientName=reader.nextLine();
+            System.out.println("Enter the client description \n");
+           String clientDesc=reader.nextLine();
+           // String query="Insert into project_management values(5,Benter,Sql programming)"+".clients";
+           String query = " insert into clients (client_name, client_description)"
+        + "values (?,?)";
+ 
+           try
+            {
+               PreparedStatement  preparedStmt= conn.prepareStatement(query);
+                    preparedStmt.setString (1, clientName);
+                 preparedStmt.setString (2, clientDesc);
+               
+               preparedStmt.execute();
                 
-                }
                 
             
             }catch(SQLException dberror)
